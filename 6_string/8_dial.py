275ec6778d635a357ba944f26ec284433c6198ca
Chaerin Noh <sinye2002@korea.ac.kr>
2022-08-11 16:30:41 +0900

    greentea

--- 6_string/8_dial.py
+++ 6_string/8_dial.py
@@ -1,13 +1,19 @@
 str = list(input())
 import string
 upper_list = list(string.ascii_uppercase)
-time = []
-for i in str:
-    for n in range(7):
-        if i in upper_list[3*n:3*n+3]:
-            x = n + 3
-            time.append(x)
-    if i in upper_list[21:26]:
-        x = 10
-        time.append(x)
-print(sum(time))
+time = 0
+
+for c in str:
+    for n in range(5):
+        if c in upper_list[3*n:3*n+3]:
+            time += n + 3
+    if c in upper_list[15:19]:
+        time += 8
+    if c in upper_list[19:22]:
+        time += 9
+    if c in upper_list[-4:]:
+        time += 10
+nums = "22233344455566677778889999"
+
+sum([int(nums[ord(c)-65]) for c in str])
+print(time)
--- 9_recursion/11729_hanoi.py
+++ 9_recursion/11729_hanoi.py
@@ -3,11 +3,10 @@ N = int(input())
 def hanoi(f, e, n):
     if n == 1:
         return [str(f)+" "+str(e)]
-    l = []
+
     m = 6 - (f + e)
-    l += hanoi(f, m, n - 1)
-    l += (hanoi(f, e, 1))
-    l += (hanoi(m, e, n - 1))
-    return l
-print(len(hanoi(1, 3, N)))
-print("\n".join([str(i) for i in hanoi(1, 3, N)]))
+    return hanoi(f, m, n - 1)+ hanoi(f, e, 1)+ hanoi(m, e, n - 1)
+
+result = hanoi(1, 3, N)
+print(len(result))
+print("\n".join([str(i) for i in result]))

721f11095433a4032a11704bf27f6211f77d7e48
Chaerin Noh <109055782+chelsh@users.noreply.github.com>
2024-07-10 12:02:54 +0900

    [Bronze II] Title: 분산처리, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,73 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    a, b = sys.stdin.readline().split()
+
+    if a[-1] == '1':
+        print(1)
+    if a[-1] == '2': 
+        b = b[-2:]
+        idx = int(b) % 4
+        if idx == 1:
+            print(2)
+        if idx == 2:
+            print(4)
+        if idx == 3:
+            print(8)
+        if idx == 0:
+            print(6)
+    if a[-1] == '3':
+        b = b[-2:]
+        idx = int(b) % 4
+        if idx == 1:
+            print(3)
+        if idx == 2:
+            print(9)
+        if idx == 3:
+            print(7)
+        if idx == 0:
+            print(1)
+    if a[-1] == '4': 
+        b = b[-1]
+        idx = int(b) % 2
+        if idx == 0:
+            print(6)
+        if idx == 1:
+            print(4)
+    if a[-1] == '5': 
+        print(5)
+    if a[-1] == '6': 
+        print(6)
+    if a[-1] == '7': 
+        b = b[-2:]
+        idx = int(b) % 4
+        if idx == 1:
+            print(7)
+        if idx == 2:
+            print(9)
+        if idx == 3:
+            print(3)
+        if idx == 0:
+            print(1)
+    if a[-1] == '8': 
+        b = b[-2:]
+        idx = int(b) % 4
+        if idx == 1:
+            print(8)
+        if idx == 2:
+            print(4)
+        if idx == 3:
+            print(2)
+        if idx == 0:
+            print(6)
+    if a[-1] == '9': 
+        b = b[-1]
+        idx = int(b) % 2
+        if idx == 0:
+            print(1)
+        if idx == 1:
+            print(9)
+    if a[-1] == '0': 
+        print(10)
